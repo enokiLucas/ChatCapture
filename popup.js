febb31a513eeda3ddc1e231aa72385df7fd98dcd
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-21 15:39:06 -0300

    JavaScript file that handles the logic for the extension popup Such as reading user input and triggering the PDF creation process.

---
+++ popup.js
@@ -0,0 +1,21 @@
+document.getElementById('export-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const contentOption = document.getElementById('content-select').value;
+  const rangeStart = document.getElementById('range-start').value;
+  const rangeEnd = document.getElementById('range-end').value;
+
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content-script.js']
+  });
+
+  chrome.tabs.sendMessage(tab.id, {
+    action: 'create-pdf',
+    contentOption,
+    rangeStart,
+    rangeEnd
+  });
+});
