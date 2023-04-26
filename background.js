d9db5c493443155290a093c7fb8c5e1c19177b75
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-26 17:07:34 -0300

    Rewrite 'background.js'

--- background.js
+++ background.js
@@ -1,21 +1,23 @@
-// Listen for messages from the content script
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'create-pdf') {
-    // Send a message to the content script to start the PDF creation process
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'create-pdf' });
+chrome.runtime.onInstalled.addListener(async () => {
+  const allTabs = await chrome.tabs.query({});
+  allTabs.forEach((tab) => {
+    if (tab.url.startsWith('https://chat.openai.com')) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['libs/jspdf.umd.min.js', 'scripts/content.js'],
+      });
+    }
+  });
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === 'complete' &&
+    tab.url.startsWith('https://chat.openai.com')
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['libs/jspdf.umd.min.js', 'scripts/content.js'],
     });
   }
-});
-
-// Listen for clicks on the extension's browser action (icon)
-chrome.action.onClicked.addListener((tab) => {
-  // Send a message to the content script to open the extension popup
-  chrome.tabs.sendMessage(tab.id, { action: 'open-popup' });
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.executeScript(tab.id, {
-    file: 'scripts/content.js',
-  });
 });
